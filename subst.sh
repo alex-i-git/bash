05fc0052192f1f1569483af7c6dbfe8897235211
alex-i-git <iostat.bsd@gmail.com>
2024-12-21 21:29:44 +0300

    arg files list added

--- subst.sh
+++ subst.sh
@@ -2,13 +2,33 @@
 
 if [ $# -eq 0 ]
   then
-    echo "Usage: $0 filename"
+    echo "Usage: $0 filename1 filename2 .. filename9"
     exit 1
 fi
 
-source parameters
-
+render() {
 while read line
 do
-    eval echo "$line"
+    eval echo "$line" >> out/$1
 done < $1
+}
+
+#Parameters filename
+source parameters
+
+
+if [ ! -d out ]; then
+  mkdir out
+fi
+
+
+for i in $@
+do
+
+    render $i
+done
+
+
+ 
+
+
